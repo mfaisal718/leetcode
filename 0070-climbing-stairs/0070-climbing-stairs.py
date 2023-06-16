e149cc100c1d9d6e5ff6836c44bcc22b88940516
Mohammed Faisal <89314352+mfaisal718@users.noreply.github.com>
2023-06-16 19:12:04 -0400

    Time: 3 ms (99.93%), Space: 13.4 MB (40.43%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def climbStairs(self, n):
+        if n <= 3:
+            return n
+        n1, n2 = 2, 3
+
+        for i in range(4, n + 1):
+            temp = n1 + n2
+            n1 = n2
+            n2 = temp
+        return n2
+
+        """
+        :type n: int
+        :rtype: int
+        """
+        

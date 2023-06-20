a75e826c195c934c42dc9aefadce2cad49aaa789
Mohammed Faisal <89314352+mfaisal718@users.noreply.github.com>
2023-06-20 12:56:17 -0400

    Time: 23 ms (27.21%), Space: 13.4 MB (45.35%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def rob(self, nums):
+        rob1, rob2 = 0,0
+        
+        for n in nums:
+            temp = max(n+rob1, rob2)
+            rob1 = rob2 
+            rob2 = temp
+            
+        return rob2
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        
